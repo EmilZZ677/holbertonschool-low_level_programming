5715052d9e8a3d13a009f77ad40129cf96f6580e
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-12 09:37:58 +0000

    emil

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#!/bin/bash
-gcc -c
 #include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 if successful
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle);
-	return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
 		
