bf4932c02a31d1c0f893e88fd1bf5e3837e462b9
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-15 14:09:57 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar('0' + i);
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
