337383d86ac3c3068c23eedc2133ee2727d0bdcd
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-15 12:59:29 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-alphabet
+ * Return:if successful 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '10')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
