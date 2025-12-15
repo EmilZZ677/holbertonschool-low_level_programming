f0b028600f299642798ae47fbc8df6369a868b5f
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-15 12:16:38 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main-alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+{
+	char d = 'A';
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
+
