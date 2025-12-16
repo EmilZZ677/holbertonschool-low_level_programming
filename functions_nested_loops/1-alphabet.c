8a4de9005bf01c31f5be596b7480143216358b8f
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-16 12:35:47 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include "main.h"
+
+/**
+* print alphabet-entry point
+* Return: always 0
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n')
+}
+
