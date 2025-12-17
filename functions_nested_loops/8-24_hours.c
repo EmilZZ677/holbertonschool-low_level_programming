ad80ba17341a69c6d293620bbf4071af13c56913
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-17 17:48:13 +0000

    a

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#incldue "main.h"
+
+/**
+* jack-bauer - bibibi
+* Return: bubububub 0
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour ++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
+}
