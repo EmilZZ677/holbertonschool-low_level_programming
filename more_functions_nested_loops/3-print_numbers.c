36c31e07dcd38e2a7ee66c3885a57f3d2a298a0c
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-22 13:04:27 +0000

    a

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers - entry point
+* @void: parameter
+* No Returnt brat
+*/
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+		_putchar(n + '0');
+}
