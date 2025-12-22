adde16616900f56945c21c6b9989b54852bc5853
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-22 13:54:12 +0000

    a

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n = 0 || n < 0)
+	{
+		_putchar('\n')
+	}
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+			_putchar(' ');
+		_putchar('/');
+	}
+
+}
