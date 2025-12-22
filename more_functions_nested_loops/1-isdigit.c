2993b1dd19936e3b591420afc1627b512878cbe2
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-22 12:38:06 +0000

    a

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* Return: 0
-* @c: tolko qazzz
-* _isdigit - ondan
+* Return: if all good and healthy it gives us 1
+* @c: parametrler
+* _isdigit - returnt if c is digit
 */
 
 int _isdigit(int c)
