f27c634af7f6a3363536b0cb27ab3dd388529d31
EmilZZ677 <emin.qurbanli08@gmail.com>
2025-12-26 12:17:26 +0000

    a

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
+ * _strcat - append to dest
+ * @dest: destination
+ * @src: source
  *
- *
- *
- *
+ * Return: returns pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int len = 0, i = 0;
 
+	while (dest[len] != '\0')
+		len++;
+	while (dest[len] == src[i])
+	{
+		len++;
+		i++;
+	}
+	return (dest);
+}
